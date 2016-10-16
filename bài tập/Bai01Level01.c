21885854289077f66a8136aaa4864a6bd7429e5d
Lê Tú Trinh <letutrinh.ktmm@gmail.com>
2016-10-16 15:41:13 +0700

    Update and rename HomeWork/README.md to  bài tập/Bai01Level01.c

---
+++ bài tập/Bai01Level01.c
@@ -0,0 +1,17 @@
+//tinh chu vi, dien tich tam giac
+#include<stdio.h>
+#include<math.h>
+main()
+{
+    float a,b,c,h,chuVi,dienTich, chieuCao,day;
+    printf("Nhap ba canh cua tam giac:\n");
+    scanf("%f%f%f",&a,&b,&c);
+    chieuCao=h;
+    printf("Nhap chieu cao cua tam giac:\n");
+    scanf("%f/n", &h);
+    chuVi=a+b+c;
+    day=c;
+    dienTich=(c*h)/2;
+    printf("Chu vi tam giac la: %0.2f\n",chuVi);
+    printf("Dien tich tam giac la: %0.2f",dienTich);
+}
